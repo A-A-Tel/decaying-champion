5e00efc276cafc6f12281f1a9a016e68e825a2ce
AAT <anouaranthonytel@gmail.com>
2025-02-04 23:21:20 +0100

    Vague weapon concept

---
+++ scripts/WeaponThrow.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+namespace DecayingChampion.scripts;
+
+public partial class WeaponThrow : Sprite2D
+{
+    public override void _Process(double delta)
+    {
+        var mouseVector = GetLocalMousePosition();
+        var angle = Mathf.Atan2(mouseVector.Y, mouseVector.X);
+        
+        Rotation = angle;
+    }
+}
