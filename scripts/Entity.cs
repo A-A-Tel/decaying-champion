acfc1aac9583e9e35dffe8a8a6b55ef815f2f7af
AAT <anouaranthonytel@gmail.com>
2025-02-09 14:13:05 +0100

    Dynamic sprite animations added

--- scripts/Entity.cs
+++ scripts/Entity.cs
@@ -9,20 +9,38 @@ public partial class Entity : CharacterBody2D
     public short Damage { get; protected set; } = 100;
     
     protected virtual float Speed { get; set; } = 100f;
-    protected byte Animation = 0;
-    protected bool AnimationFrame = false;
-    protected bool IsMoving = false;
+    protected byte AnimationCount;
+    protected bool HasAnimation = true;
+    protected bool IsMoving;
 
     protected Sprite2D Sprite;
     protected Timer AnimationTimer;
+
+    private byte _currentAnimation = 1;
+    
+    private Vector2 _animationSize;
 
     public override void _PhysicsProcess(double delta)
     {
         CheckIfDead();
         Move();
-        SetAnimation();
-        PlayAnimation();
+        if (HasAnimation)
+        {
+            SetAnimation();
+            PlayAnimation();
+        }
         MoveAndCollide(Velocity * (float)delta);
+    }
+
+    protected void CalculateTextureCount()
+    {
+        var textureSize = Sprite.Texture.GetSize();
+
+        _animationSize = new Vector2(
+            textureSize.X / (AnimationCount + 1f),
+            textureSize.Y / 8f
+        );
+        GD.Print(_animationSize);
     }
 
     protected virtual void CheckIfDead()
@@ -36,7 +54,61 @@ public partial class Entity : CharacterBody2D
     
     protected virtual void Move() {}
 
-    protected virtual void SetAnimation() {}
+    private void SetAnimation()
+    {
+        if (IsMoving)
+        {
+            var direction = (byte)GetMovementDirection();
 
-    protected virtual void PlayAnimation() {}
+            Sprite.RegionRect = new Rect2(0f, _animationSize.Y * direction, _animationSize);
+        }
+    }
+
+    private void PlayAnimation()
+    {
+        if (IsMoving)
+        {
+            Sprite.RegionRect = new Rect2(_animationSize.X * _currentAnimation, Sprite.RegionRect.Position.Y, _animationSize);
+
+            if (AnimationTimer.IsStopped())
+            {
+                _currentAnimation++;
+                if (_currentAnimation > AnimationCount) _currentAnimation = 1;
+                AnimationTimer.Start();
+            }
+        }
+        else
+        {
+            Sprite.RegionRect = new Rect2(0, Sprite.RegionRect.Position.Y, _animationSize);
+        }
+    }
+    
+    private Direction GetMovementDirection()
+    {
+
+        var angle = Mathf.RadToDeg(Velocity.Angle());
+
+        if (angle >= -112.5f && angle < -67.5f) return Direction.Up;
+        if (angle >= 157.5f || angle < -157.5f) return Direction.Left;
+        if (angle >= 67.5f && angle < 112.5f) return Direction.Down;
+        if (angle >= -22.5f && angle < 22.5f) return Direction.Right;
+        if (angle >= -157.5f && angle < -112.5f) return Direction.UpLeft;
+        if (angle >= 112.5f && angle < 157.5f) return Direction.DownLeft;
+        if (angle >= 22.5f && angle < 67.5f) return Direction.DownRight;
+        if (angle >= -67.5f && angle < -22.5f) return Direction.UpRight;
+
+        return Direction.Down;
+    }
+
+    private enum Direction
+    {
+        Up,
+        Left,
+        Down,
+        Right,
+        UpLeft,
+        DownLeft,
+        DownRight,
+        UpRight
+    }
 }
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -11,79 +11,41 @@ public partial class Player : Entity
         ResetValues();
         Sprite = GetNode<Sprite2D>("Sprite2D");
         AnimationTimer = GetNode<Timer>("AnimationTimer");
+        AnimationCount = 2;
+        CalculateTextureCount();
     }
 
     protected override void Move()
     {
         Velocity = Vector2.Zero;
         IsMoving = false;
-
-        var up = false;
-        var left = false;
-        Animation = 0;
         
         if (Input.IsActionPressed("MoveUp"))
         {
             Velocity += Vector2.Up;
-            Animation += 4;
-            up = true;
         }
 
         if (Input.IsActionPressed("MoveLeft"))
         {
             Velocity += Vector2.Left;
-            Animation += 2;
-            left = true;
         }
 
         if (Input.IsActionPressed("MoveDown"))
         {
             Velocity += Vector2.Down;
-            if (up) Animation -= 4; else Animation += 1;
         }
 
         if (Input.IsActionPressed("MoveRight"))
         {
             Velocity += Vector2.Right;
-            if (left) Animation -= 2; else Animation += 7;
         }
+        
+        if (!Velocity.Equals(Vector2.Zero)) IsMoving = true;
         Velocity = Velocity.Normalized() * Speed;
     }
 
     public void ResetValues()
     {
         Health = MaxHealth;
-    }
-
-    protected override void SetAnimation()
-    {
-        if (Animation != 0)
-        {
-            IsMoving = true;
-            Sprite.RegionRect = new Rect2(0f, 0f + (Animation - 1f) * 38f, 30f, 38f);
-        }
-        else
-        {
-            IsMoving = false;
-        }
-    }
-
-    protected override void PlayAnimation()
-    {
-        if (!IsMoving)
-        {
-            AnimationFrame = false;
-            Sprite.RegionRect = new Rect2(0f, Sprite.RegionRect.Position.Y, 30f, 38f);
-            return; 
-        }
-        
-            var xOffset = AnimationFrame ? 60f : 30f;
-            Sprite.RegionRect = new Rect2(xOffset, Sprite.RegionRect.Position.Y, 30f, 38f);
-
-        if (AnimationTimer.IsStopped())
-        {
-            AnimationFrame = !AnimationFrame;
-            AnimationTimer.Start();
-        }
     }
 }
