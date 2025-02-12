fc64b0c861ab18068c8b001b79c0f65a3a350fd0
AAT <anouaranthonytel@gmail.com>
2025-02-12 22:31:35 +0100

    Adde sprite

--- scripts/meta/Arena.cs
+++ scripts/meta/Arena.cs
@@ -7,126 +7,124 @@ namespace DecayingChampion.scripts;
 
 public partial class Arena : StaticBody2D
 {
-	public byte Round { get; private set; }
-	public byte Wave { get; private set; }
-	public bool HasStarted { get; private set; }
-	
-	private Vector2 _playerSpawn = new(0f, 1015f);
+    public byte Round { get; private set; }
+    public byte Wave { get; private set; }
+    public bool HasStarted { get; private set; }
 
-	private Vector2[] _enemySpawns =
-	{
-		new(-835, -616),
-		new(-515, -780),
-		new(529, -780),
-		new(963, -590),
-		new(1196, -265),
-		new(1196, 378),
-		new(936, 687),
-		new(505, 865),
-		new(-460, 932),
-		new(-835, 752),
-		new(-1169, 474),
-		new(-1140, -326)
-	};
+    private Vector2 _playerSpawn = new(0f, 1015f);
 
-	private Timer _waveTimer;
-	private EnemyRounds.EnemyData[][] _round;
-	private Area2D _killZone;
-	private Player _player;
+    private Vector2[] _enemySpawns =
+    {
+        new(-835, -616),
+        new(-515, -780),
+        new(529, -780),
+        new(963, -590),
+        new(1196, -265),
+        new(1196, 378),
+        new(936, 687),
+        new(505, 865),
+        new(-460, 932),
+        new(-835, 752),
+        new(-1169, 474),
+        new(-1140, -326)
+    };
+
+    private Timer _waveTimer;
+    private EnemyRounds.EnemyData[][] _round;
+    private Area2D _killZone;
+    private Player _player;
 
 
-	public override void _Ready()
-	{
-		_waveTimer = GetNode<Timer>("WaveTimer");
-		_killZone = GetNode<Area2D>("KillZone");
-		_player = GetNode<Player>("Player");
-		StartRound();
-	}
+    public override void _Ready()
+    {
+        _waveTimer = GetNode<Timer>("WaveTimer");
+        _killZone = GetNode<Area2D>("KillZone");
+        _player = GetNode<Player>("Player");
+        StartRound();
+    }
 
-	public override void _Process(double delta)
-	{
-		if (Wave > 4)
-		{
-			HasStarted = false;
+    public override void _Process(double delta)
+    {
+        if (Wave > 4)
+        {
+            HasStarted = false;
 
-			if (IsEverythingDead())
-			{
-				if (Round % 3 != 0)
-				{
-					_player.ResetValues();
-					StartRound();
-				}
-			}
-		}
-		if (HasStarted)
-		{
-			if (IsEverythingDead())
-			{
-				SpawnWave(_round[Wave]);
-				Wave++;
-			}
-		}
-		KillEntities();
-	}
+            if (IsEverythingDead())
+            {
+                _player.ResetValues();
+                StartRound();
+            }
+        }
 
-	public void StartRound()
-	{
-		Wave = 0;
-		_round = EnemyRounds.GetRound(Round);
-		Round++;
-		HasStarted = true;
-	}
+        if (HasStarted)
+        {
+            if (IsEverythingDead())
+            {
+                SpawnWave(_round[Wave]);
+                Wave++;
+            }
+        }
 
-	private void SpawnWave(EnemyRounds.EnemyData[] wave)
-	{
-		Random rnd = new();
+        KillEntities();
+    }
 
-		foreach (EnemyRounds.EnemyData enemyData in wave)
-		{
-			GD.Print(enemyData.Enemy.Name);
-			enemyData.Enemy.AddToGroup("enemies");
+    public void StartRound()
+    {
+        Wave = 0;
+        _round = EnemyRounds.GetRound(Round);
+        Round++;
+        HasStarted = true;
+    }
 
-			for (short i = 0; i < enemyData.Amount; i++)
-			{
-				enemyData.Enemy.Position = _enemySpawns[rnd.Next(_enemySpawns.Length)];
-				AddChild(enemyData.Enemy.Duplicate());
-			}
-		}
+    private void SpawnWave(EnemyRounds.EnemyData[] wave)
+    {
+        Random rnd = new();
 
-		if (_waveTimer.IsStopped())
-		{
-			_waveTimer.Start();
-		}
-		else
-		{
-			_waveTimer.Stop();
-			_waveTimer.Start();
-		}
-	}
+        foreach (EnemyRounds.EnemyData enemyData in wave)
+        {
+            GD.Print(enemyData.Enemy.Name);
+            enemyData.Enemy.AddToGroup("enemies");
 
-	private bool IsEverythingDead()
-	{
-		foreach (Node node in GetChildren())
-		{
-			if (node.IsInGroup("enemies")) return false;
-		}
+            for (short i = 0; i < enemyData.Amount; i++)
+            {
+                enemyData.Enemy.Position = _enemySpawns[rnd.Next(_enemySpawns.Length)];
+                AddChild(enemyData.Enemy.Duplicate());
+            }
+        }
 
-		return true;
-	}
+        if (_waveTimer.IsStopped())
+        {
+            _waveTimer.Start();
+        }
+        else
+        {
+            _waveTimer.Stop();
+            _waveTimer.Start();
+        }
+    }
 
-	private void KillEntities()
-	{
-		foreach (Node2D node in _killZone.GetOverlappingBodies())
-		{
-			if (node is Entity entity)
-			{
-				entity.TerminateChild();
-			}
-		}
-	}
+    private bool IsEverythingDead()
+    {
+        foreach (Node node in GetChildren())
+        {
+            if (node.IsInGroup("enemies")) return false;
+        }
 
-	private void TerminateChildScenes()
-	{
-		
-	}
+        return true;
+    }
+
+    private void KillEntities()
+    {
+        foreach (Node2D node in _killZone.GetOverlappingBodies())
+        {
+            if (node is Entity entity)
+            {
+                entity.TerminateChild();
+            }
+        }
+    }
+
+    private void TerminateChildScenes()
+    {
+    }
 }
